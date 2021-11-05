431d461c319e8546abfa10dfce8e18acdf2580d2
Noel Leahy <nleahy1234@gmail.com>
2021-11-05 16:17:41 +0000

    E2E tests - Navigation - Backward/Forward between the Favourite movies page and movie details page.

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -71,4 +71,26 @@ describe("Navigation", () => {
         cy.get("h3").contains(movies[0].title);
         });
   });
+
+  describe("The forward/backward links", () => {
+    beforeEach(() => {
+      cy.get(".MuiCardActions-root").eq(0).contains("More Info").click();
+    });
+    it("should navigate backward and forward between the movies detail page and the Discover page.", () => {
+      cy.get("button[aria-label='go back'").click();
+      cy.get("h3").contains("Discover Movies");
+      cy.url().should("not.include", `/movies/${movies[0].id}`);
+      cy.get("button[aria-label='go forward'").eq(1).click();
+      cy.url().should("include", `/movies/${movies[0].id}`);
+      cy.get("h3").contains(movies[0].title);
+    });
+    it("forward/backward links between Favouite movies and Movie details", () => {
+      cy.get(".MuiButton-colorInherit").eq(1).click();
+      cy.get("button[aria-label='go back'").eq(0).click();
+      cy.url().should("include", `/movies/${movies[0].id}`);
+      cy.get("button[aria-label='go forward'").eq(0).click();
+      cy.url().should("include", `/favorites`);
+    });
+  });
+ 
 });
